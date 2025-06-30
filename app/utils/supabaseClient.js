618d481eb9d9e056e889228a4b258a77a33e7ceb
Lethabu <Thabangt01@gmail.com>
2025-06-30 15:00:47 +0200

    Update supabaseClient.js

--- app/utils/supabaseClient.js
+++ app/utils/supabaseClient.js
@@ -1,4 +1,8 @@
-// utils/supabaseClient.js
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createBrowserSupabaseClient();
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+// Note: This client is for client-side usage only where RLS is enforced.
+// For server-side logic, use the helpers (`createServerActionClient`, etc.).
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
