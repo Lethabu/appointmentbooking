6b37713ba5cb1765611032039d7e21854b7a54b2
Lethabu <thabangt01@gmail.com>
2025-07-05 06:19:46 +0200

    icon update

--- app/components/Icons/SparklesIcon.tsx
+++ app/components/Icons/SparklesIcon.tsx
@@ -4,10 +4,19 @@ export const SparklesIcon: React.FC<{ className?: string }> = ({ className }) =>
   <svg
     className={className}
     xmlns="http://www.w3.org/2000/svg"
-    fill="none"
+    width="24"
+    height="24"
     viewBox="0 0 24 24"
+    fill="none"
     stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
   >
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-6.857 2.286L12 21l-2.286-6.857L3 12l6.857-2.286L12 3z" />
+    <path d="M12 3L14.34 8.66L20 11L14.34 13.34L12 19L9.66 13.34L4 11L9.66 8.66L12 3Z" />
+    <path d="M5 3L6 5" />
+    <path d="M19 3L18 5" />
+    <path d="M5 19L6 17" />
+    <path d="M19 19L18 17" />
   </svg>
 );
