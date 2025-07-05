dbf50dc966544c0d79f54bbeaeb3e6f3b6613800
Lethabu <thabangt01@gmail.com>
2025-07-05 04:15:52 +0200

    build log

--- app/components/Icons/SparklesIcon.tsx
+++ app/components/Icons/SparklesIcon.tsx
@@ -1,4 +1,13 @@
 import React from 'react';
 
 export const SparklesIcon: React.FC<{ className?: string }> = ({ className }) => (
-  <svg className={className} xmlns="http://www.w3.org/
+  <svg
+    className={className}
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-6.857 2.286L12 21l-2.286-6.857L3 12l6.857-2.286L12 3z" />
+  </svg>
+);
