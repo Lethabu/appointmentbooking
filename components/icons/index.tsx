f340e1906f253f7951ab153d52eb7caaf3062087
Lethabu <thabangt01@gmail.com>
2025-07-08 20:13:13 +0200

    Add proper TypeScript types to the icon components

--- components/icons/index.tsx
+++ components/icons/index.tsx
@@ -7,7 +7,7 @@ import React from 'react';
  * It accepts standard SVG props like className for styling.
  */
 interface IconProps extends React.SVGProps<SVGSVGElement> {
-  size?: number; // Example of a custom prop
+  size?: number;
 }
 
 export type Icon = React.FC<IconProps>;
@@ -30,13 +30,13 @@ export const IconWhatsApp: Icon = (props) => (
 );
 // If you had other icons, you would export them here as well, for example:
 
-export const IconCalendar = () => (
+export const IconCalendar: Icon = (props) => (
   <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
   </svg>
 );
 
-export const IconChat = () => (
+export const IconChat: Icon = (props) => (
   <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
   </svg>
@@ -48,7 +48,7 @@ export const IconSend = () => (
   </svg>
 );
 
-export const IconSparkles = () => (
+export const IconSparkles: Icon = (props) => (
   <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.571L14 21m-3-3V5m-2.428 5.714l-.714 2.143" />
   </svg>
