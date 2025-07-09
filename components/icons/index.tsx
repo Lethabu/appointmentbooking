6ce32bddf0073cd45cbafa39a1fb07282dd6304e
Lethabu <thabangt01@gmail.com>
2025-07-09 02:40:01 +0200

    Let's fix the `IconChevronDown` component formatting and prop handling:

--- components/icons/index.tsx
+++ components/icons/index.tsx
@@ -60,8 +60,17 @@ export const IconUserCircle: Icon = (props) => (
   </svg>
 );
 
-export const IconChevronDown: Icon = (props) => (
-<svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
-</svg>
+export const IconChevronDown: Icon = ({ size = 24, ...props }) => (
+  <svg
+    width={size}
+    height={size}
+    className="w-6 h-6"
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+  </svg>
 );
