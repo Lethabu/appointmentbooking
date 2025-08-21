db7bf364056d5bf9ecf348cef8cd434f5df95358
Lethabu <thabangt01@gmail.com>
2025-08-21 16:27:56 +0200

    removed the non-standard export { reportWebVitals } from './vitals'; from app/layout.tsx as a potential fix for the build erro

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import './globals.css'
 import { ConvexClientProvider } from './providers'
 import { ClerkProvider } from '@clerk/nextjs'
 import { CSPostHogProvider } from '@/components/PostHogProvider'
-export { reportWebVitals } from './vitals';
 
 const inter = Inter({ subsets: ['latin'] });
 
