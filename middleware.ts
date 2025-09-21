66b5c84054b1dc4c72c7a189e648b4791613eb49
thabangt01 <thabangt01@gmail.com>
2025-09-21 16:37:40 +0000

    fix: Update middleware matcher to exclude all static assets

--- middleware.ts
+++ middleware.ts
@@ -36,6 +36,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    '/((?!_next|api|favicon.ico|.*\\.).*)',
   ]
 };
